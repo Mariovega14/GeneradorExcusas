f5597ae94a0a5e9a1888ae60e0bf8e06ee0129d1
Mario Vega <marioalbe123@gmail.com>
2024-09-13 21:27:44 +0000

    primet try

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const mostradorExcusa = document.getElementById("mostradorExcusa");
+
+function generadorExcusa() {
+  const parteWho = who[Math.floor(Math.random() * who.length)];
+  const parteAction = action[Math.floor(Math.random() * action.length)];
+  const parteWhat = what[Math.floor(Math.random() * what.length)];
+  const parteWhen = when[Math.floor(Math.random() * when.length)];
+
+  const excuse = `${parteWho} ${parteAction} ${parteWhat} ${parteWhen}`;
+  mostradorExcusa.textContent = excuse;
+}
+
+window.onload = generadorExcusa;
